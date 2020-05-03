7dfb3d88b55c2cc5b0e3a5b0ae5db5a5a34ea5da
Armen Markossyan <hello@armen.im>
2020-05-03 23:20:30 +0200

    Improve git aliases

--- git.sh
+++ git.sh
@@ -1,27 +1,41 @@
-# git
+# long aliases
 
-alias gcheckout="git checkout"
+alias g_checkout="git checkout"
+alias g_master="git checkout master"
+alias g_branch="git checkout -b"
+alias g_push="git push"
+alias g_commit="git commit -m"
+alias g_amend="git commit --amend --no-edit"
+alias g_force="git push --force-with-lease"
+alias g_pull="git stash --include-untracked; git pull --rebase; git stash pop"
+alias g_add="git add"
+alias g_status="git status"
 
-alias gmaster="git checkout master"
-alias master=gmaster
+g_add_push() {
+	git add . && git commit -m ${1} && git push
+}
 
-alias gbranch="git checkout -b"
-alias branch=gbranch
+g_clone() {
+  git clone git@github.com:$1.git $2
+}
 
-alias gpush="git push"
-alias push=gpush
+# short aliases
 
-alias gcommit="git commit -m "
-alias commit=gcommit
-
-alias gamend="git commit --ammend --no-edit"
-alias amend=gamend
-
-alias gforce="git push --force-with-lease"
-alias force=gforce
-
-alias pull="git stash --include-untracked; git pull --rebase; git stash pop"
+alias ch=g_checkout
+alias master=g_master
+alias branch=g_branch
+alias push=g_push
+alias commit=g_commit
+alias amend=g_amend
+alias force=g_force
+alias pull=g_pull
+alias add=g_add
+alias status=g_status
 
 clone() {
-  git clone git@github.com:$1.git
+  g_clone $1 $2
+}
+
+PUSH () {
+	g_add_push $1
 }
